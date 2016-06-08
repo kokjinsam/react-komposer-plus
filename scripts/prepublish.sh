06f61ee76962e987bcf19f0a88546da0fb9d920f
skaxwng <samkj.ks@gmail.com>
2016-06-08 16:59:33 -0500

    Published v2.0.6

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -1,5 +1,6 @@
 echo "> Start transpiling ES2015"
 echo ""
+rm -rf ./lib
 babel --plugins "transform-runtime" src --ignore __tests__ --out-dir ./lib
 cd lib
 browserify --debug --ignore-missing -t [ exposify --expose [ --react React ] ] ./window_bind.js > ./browser.js
